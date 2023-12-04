9408b98cf4bafec3e8ed8598aaa9b40b793986f3
Tomohiro Yoshida <tomohiroyoshida10@gmail.com>
2023-12-03 17:46:28 -0800

    Setup electron dev

---
+++ packages/react-frontend/src/main/main.js
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron';
+// import * as path from 'path';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({});
+
+  // Vite dev server URL
+  mainWindow.loadURL('http://localhost:5173');
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  /* eslint-disable-next-line no-undef */
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow == null) {
+    createWindow();
+  }
+});
